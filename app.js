b81598e9d5214d3f80065d9ef043ca9168576f33
Paul Stevens <twinkisan@hotmail.com>
2020-09-26 16:56:03 -0700

    adds error handling in case of empty inputs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const verb1 = document.getElementById("verb1");
 const number1 = document.getElementById("number1");
 const adjective1 = document.getElementById("adjective1");
 const number2 = document.getElementById("number2");
-const number2a = document.getElementById("number2a");
+//const number2a = document.getElementById("number2a");
 const adjective3 = document.getElementById("adjective3");
 const submitButton = document.getElementById("submit");
 const resetButton = document.getElementById("reset");
@@ -30,37 +30,52 @@ const outputAdjective3 = document.getElementById("output-adjective3");
 
 const inputForm = document.getElementById("input-form");
 const outputForm = document.getElementById("output-form");
-
 const machinePicture = document.getElementById("machine-picture");
 
 // issues: injecting the first number into the first noun;
 // though the second repeat noun accurately repeats in the third slot
 
-submitButton.addEventListener("click", () => {
-  // replacing the text content of the output with the input value
-  outputNoun1.textContent = noun1.value;
-  outputNoun1a.textContent = noun1.value;
-  outputNoun1b.textContent = noun1.value;
-  outputNoun1c.textContent = noun1.value;
-  outputNoun2.textContent = noun2.value;
-  outputNoun3.textContent = noun3.value;
-  outputVerb1.textContent = verb1.value;
-  outputNumber1.textContent = number1.value;
-  outputNumber2.textContent = number2.value;
-  outputNumber2a.textContent = number2.value;
-  outputAdjective1.textContent = adjective1.value;
-  outputAdjective3.textContent = adjective3.value;
+function validateInput() {
+  if (noun1.value === "" ||
+    noun2.value === "" ||
+    noun3.value === "" ||
+    verb1.value === "" ||
+    number1.value === "" ||
+    adjective1.value === "" ||
+    number2.value === "" ||
+    adjective3.value === ""
+  ){
+    alert("You left one blank!")
+  } else {
+    createPoem();
+  }
+}
 
-  // toggle class hidden and visible onsubmit
-  inputForm.classList.replace("visible", "hidden");
-  outputForm.classList.replace("hidden", "visible");
-  outputForm.classList.add("poem-format");
 
-  machinePicture.classList.add("img-fadein");
-});
 
-resetButton.addEventListener("click", () => {
-  // replacing the text content of the output with the input value
+function createPoem() {
+    // replacing the text content of the output with the input value
+    outputNoun1.textContent = noun1.value;
+    outputNoun1a.textContent = noun1.value;
+    outputNoun1b.textContent = noun1.value;
+    outputNoun1c.textContent = noun1.value;
+    outputNoun2.textContent = noun2.value;
+    outputNoun3.textContent = noun3.value;
+    outputVerb1.textContent = verb1.value;
+    outputNumber1.textContent = number1.value;
+    outputNumber2.textContent = number2.value;
+    outputNumber2a.textContent = number2.value;
+    outputAdjective1.textContent = adjective1.value;
+    outputAdjective3.textContent = adjective3.value;
+  
+    // toggle class hidden and visible onsubmit
+    inputForm.classList.toggle("hidden");
+    outputForm.classList.toggle("hidden");
+    outputForm.classList.add("poem-fadein");
+    machinePicture.classList.add("img-fadein");
+}
+
+function resetPoem() {
   outputNoun1.textContent = "";
   outputNoun1a.textContent = "";
   outputNoun1b.textContent = "";
@@ -74,12 +89,16 @@ resetButton.addEventListener("click", () => {
   outputAdjective1.textContent = "";
   outputAdjective3.textContent = "";
 
-  // toggle class hidden and visible onsubmit
-  inputForm.classList.replace("hidden", 'visible');
-  outputForm.classList.replace("visible", "hidden");
-  outputForm.classList.remove("poem-format");
+  // toggle class hidden and visible onreset
+  inputForm.classList.toggle("hidden");
+  outputForm.classList.toggle("hidden");
+  outputForm.classList.remove("poem-fadein");
 
   machinePicture.classList.remove("img-fadein");
-});
+}
+
+submitButton.addEventListener("click", validateInput);
+
+resetButton.addEventListener("click", resetPoem);
 
 
