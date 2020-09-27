b6ae90d0fa0f472d1d0e1b0395d261f93d82ce97
kevinfiero <kjf46@cornell.edu>
2020-09-27 13:26:00 -0700

    Highlight missed input boxes in red

--- utils/clickHandlers.js
+++ utils/clickHandlers.js
@@ -1,22 +1,56 @@
 import { createPoem } from './poemPublisher.js';
+import { resetBorders } from './reset.js';
 
 export function validateInput() {
-    if (noun1.value === 'test'){
-        createPoem();
-    }  
-    else if (noun1.value === '' ||
-      noun2.value === '' ||
-      noun3.value === '' ||
-      verb1.value === '' ||
-      number1.value === '' ||
-      adjective1.value === '' ||
-      number2.value === '' ||
-      adjective3.value === ''
-    ){
-        alert('You left one blank!');
+    let counter = 0;
+    let message = '';
+
+    resetBorders();
+
+    if (noun1.value === ''){
+        counter++;
+        noun1.style.border = '2px solid red';
+    }
+    if (noun2.value === ''){
+        counter++;
+        noun2.style.border = '2px solid red';
+    }
+    if (noun3.value === ''){
+        counter++;
+        noun3.style.border = '2px solid red';
+    }
+    if (verb1.value === ''){
+        counter++;
+        verb1.style.border = '2px solid red';
+    }
+    if (number1.value === ''){
+        counter++;
+        number1.style.border = '2px solid red';
+    }
+    if (adjective1.value === ''){
+        counter++;
+        adjective1.style.border = '2px solid red';
+    }
+    if (adjective2.value === ''){
+        counter++;
+        adjective2.style.border = '2px solid red';
+    }
+    if (number2.value === ''){
+        counter++;
+        number2.style.border = '2px solid red';
+    }
+    if (adjective3.value === ''){
+        counter++;
+        adjective3.style.border = '2px solid red';
+    }
+
+    if (counter === 1){
+        message = 'You left 1 field blank!';
+        alert(message);
+    } else if (counter > 1){
+        message = 'You left ' + counter + ' fields blank!';
+        alert(message);
     } else {
         createPoem();
-    }
+    } 
 }
-
-  
